41a0465594c207360b7721333700bec4bd7d1bf2
Barry Williams <barry.williams@linn.co.uk>
2016-11-29 10:02:17 +0000

    Couple of certification and deployment issues resolved

--- generateCertificate.sh
+++ generateCertificate.sh
@@ -4,4 +4,4 @@ aws iot create-keys-and-certificate \
 	--set-as-active \
 	--certificate-pem-outfile=./certs/certificate.pem.crt \
 	--public-key-outfile=./certs/public.pem.key \
-	--private-key-outfile=./certs/private.pem.key
+	--private-key-outfile=./certs/private.pem.key > ./certs/certificateAndKeys.json
