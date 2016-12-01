b7908a5139b1cb57595d89ff625131109c515a93
Barry Williams <barry.williams@linn.co.uk>
2016-12-01 14:18:50 +0000

    Added helper script

--- generateCertificate.sh
+++ generateCertificate.sh
@@ -5,3 +5,5 @@ aws iot create-keys-and-certificate \
 	--certificate-pem-outfile=./certs/certificate.pem.crt \
 	--public-key-outfile=./certs/public.pem.key \
 	--private-key-outfile=./certs/private.pem.key > ./certs/certificateAndKeys.json
+
+node -e "console.log(require('./certs/certificateAndKeys.json').certificateArn)" > ./certs/arn.txt
