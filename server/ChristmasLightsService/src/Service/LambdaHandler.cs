66886f816a089bed7fc24a75c8d26292b13109b0
Barry Williams <barry.williams@linn.co.uk>
2016-12-06 09:39:56 +0000

    Initial C# lambda backend

---
+++ server/ChristmasLightsService/src/Service/LambdaHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace Linn.ChristmasLights.Service
+{
+    using System.IO;
+
+    using Amazon.Lambda.Core;
+
+    public class LambdaHandler
+    {
+        public Stream Handler(Stream inputStream, ILambdaContext context)
+        {
+            context.Logger.LogLine("Not implemented");
+            return null;
+        }
+    }
+}
